107c5d6172fdb839afd54e3124a9e406123a73ca
Pavan Chindukuri <141718713+pavan-intelops@users.noreply.github.com>
2024-02-01 16:26:16 +0530

    Login page (#3)
    * adding new login route
    
    * adding vscode settings file for auto-complete and adding AddOrLoadProject Section

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,77 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Container, Flex, Space, Text } from '@mantine/core'
+import { useForm } from 'react-hook-form'
+import { TextInput } from 'react-hook-form-mantine'
+import { useNavigate } from 'react-router-dom'
+import useUserStore from 'src/store/userStore'
+import * as z from 'zod'
+
+const resolver = z.object({
+	email: z.string().email(),
+})
+
+export default function Login() {
+	const form = useForm({
+		defaultValues: {
+			email: '',
+		},
+		mode: 'all',
+		resolver: zodResolver(resolver),
+		shouldFocusError: true,
+		shouldUseNativeValidation: false,
+	})
+	const { setPersonalDetails } = useUserStore()
+	const navigate = useNavigate()
+
+	const handleSubmit = form.handleSubmit((data) => {
+		setPersonalDetails({
+			email: data.email,
+		})
+		navigate('/')
+	})
+	return (
+		<Container
+			mt='auto'
+			size='xs'
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				height: '100vh',
+			}}
+		>
+			<Flex
+				bg='gray.7'
+				w='100%'
+				direction='column'
+				align='flex-start'
+				justify='flex-start'
+				p='xl'
+				rowGap='sm'
+			>
+				<Text size='xl' fw='bold' td='underline' ta='left'>
+					Add your email to create projects
+				</Text>
+				<form
+					onSubmit={handleSubmit}
+					onSubmitCapture={(e) => {
+						e.preventDefault()
+					}}
+				>
+					<TextInput
+						type='email'
+						placeholder='Email'
+						control={form.control}
+						name='email'
+						withAsterisk
+						label='Email'
+					/>
+					<Space h='sm' />
+					<Button type='submit' color='orange'>
+						Login
+					</Button>
+				</form>
+			</Flex>
+		</Container>
+	)
+}
