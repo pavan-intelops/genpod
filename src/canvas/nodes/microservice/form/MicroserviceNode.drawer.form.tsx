0cca22461ce22ea671f48cdeace476d9a165b3f1
Pavan Chindukuri <cpavan@intelops.dev>
2024-03-06 12:42:33 +0530

    removing redundant literal in a logical expressions

--- src/canvas/nodes/microservice/form/MicroserviceNode.drawer.form.tsx
+++ src/canvas/nodes/microservice/form/MicroserviceNode.drawer.form.tsx
@@ -1,99 +1,99 @@
-import { Button } from '@mantine/core'
-import { useForm } from 'react-hook-form'
-import { Select, TextInput, Textarea } from 'react-hook-form-mantine'
-import { useFlowsStore } from 'src/canvas/store/flowstore'
+import { Button } from '@mantine/core';
+import { useForm } from 'react-hook-form';
+import { Select, TextInput, Textarea } from 'react-hook-form-mantine';
+import { useFlowsStore } from 'src/canvas/store/flowstore';
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useCallback } from 'react'
-import { NodeTypes } from 'src/canvas/store/types.store'
-import { NodeDrawerFormProps } from 'src/canvas/types'
-import { getLanguageOptions, handleResets } from '../Microservice.utils'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useCallback } from 'react';
+import { NodeTypes } from 'src/canvas/store/types.store';
+import { NodeDrawerFormProps } from 'src/canvas/types';
+import { getLanguageOptions, handleResets } from '../Microservice.utils';
 import {
-	MicroServiceNodeFormData,
-	MicroServiceNodeFormDataUI,
-} from '../MicroserviceNode.types'
-import GRPCConfigForm from './GRPCConfigForm'
-import RestConfigForm from './RESTConfigForm'
-import { schema } from './resolvers'
+  MicroServiceNodeFormData,
+  MicroServiceNodeFormDataUI
+} from '../MicroserviceNode.types';
+import GRPCConfigForm from './GRPCConfigForm';
+import RestConfigForm from './RESTConfigForm';
+import { schema } from './resolvers';
 
 export default function MicroServiceNodeDrawerForm(
-	props: NodeDrawerFormProps<MicroServiceNodeFormData>
+  props: NodeDrawerFormProps<MicroServiceNodeFormData>
 ) {
-	const { getNodeFormData, setNodeFormData } = useFlowsStore()
-	const currentFormData = getNodeFormData(props.nodeId)
+  const { getNodeFormData, setNodeFormData } = useFlowsStore();
+  const currentFormData = getNodeFormData(props.nodeId);
 
-	const form = useForm<MicroServiceNodeFormDataUI>({
-		defaultValues: structuredClone(currentFormData),
-		resolver: zodResolver(schema),
-		reValidateMode: 'onBlur',
-		criteriaMode: 'all',
-	})
-	const transformToNodeFormData = useCallback(
-		(data: MicroServiceNodeFormDataUI): MicroServiceNodeFormData => {
-			const t: MicroServiceNodeFormData = {
-				name: data.name,
-				description: data.description,
-				language: data.language,
-				restConfig: data.restConfig || undefined,
-				grpcConfig: data.grpcConfig || undefined,
-				annotations: {},
-				id: 'form' + props.nodeId,
-				metadata: {},
-				type: NodeTypes.MICROSERVICE,
-				wsConfig: undefined,
-			}
-			return t
-		},
-		[props.nodeId]
-	)
-	return (
-		<>
-			<form
-				onSubmitCapture={(e) => {
-					e.preventDefault()
-				}}
-				onSubmit={form.handleSubmit((data) => {
-					setNodeFormData(
-						transformToNodeFormData(form.getValues()),
-						props.nodeId
-					)
-					props.onSubmit(transformToNodeFormData(data))
-				})}
-			>
-				<TextInput
-					control={form.control}
-					withAsterisk
-					label='Name'
-					placeholder='Name'
-					name='name'
-				/>
-				<Textarea
-					name='description'
-					rows={4}
-					label='Description'
-					placeholder='Description'
-					control={form.control}
-				/>
-				<Select
-					control={form.control}
-					name='language'
-					label='Language'
-					placeholder='Language'
-					data={getLanguageOptions()}
-					onChange={() => {
-						handleResets(form)
-					}}
-				/>
-				{!!form.watch('language') && (
-					<>
-						<RestConfigForm form={form} />
-						<GRPCConfigForm form={form} />
-					</>
-				)}
-				<Button type='submit' mt='lg'>
-					Submit
-				</Button>
-			</form>
-		</>
-	)
+  const form = useForm<MicroServiceNodeFormDataUI>({
+    defaultValues: structuredClone(currentFormData),
+    resolver: zodResolver(schema),
+    reValidateMode: 'onBlur',
+    criteriaMode: 'all'
+  });
+  const transformToNodeFormData = useCallback(
+    (data: MicroServiceNodeFormDataUI): MicroServiceNodeFormData => {
+      const t: MicroServiceNodeFormData = {
+        name: data.name,
+        description: data.description,
+        language: data.language,
+        restConfig: data.restConfig,
+        grpcConfig: data.grpcConfig,
+        annotations: {},
+        id: 'form' + props.nodeId,
+        metadata: {},
+        type: NodeTypes.MICROSERVICE,
+        wsConfig: undefined
+      };
+      return t;
+    },
+    [props.nodeId]
+  );
+  return (
+    <>
+      <form
+        onSubmitCapture={e => {
+          e.preventDefault();
+        }}
+        onSubmit={form.handleSubmit(data => {
+          setNodeFormData(
+            transformToNodeFormData(form.getValues()),
+            props.nodeId
+          );
+          props.onSubmit(transformToNodeFormData(data));
+        })}
+      >
+        <TextInput
+          control={form.control}
+          withAsterisk
+          label="Name"
+          placeholder="Name"
+          name="name"
+        />
+        <Textarea
+          name="description"
+          rows={4}
+          label="Description"
+          placeholder="Description"
+          control={form.control}
+        />
+        <Select
+          control={form.control}
+          name="language"
+          label="Language"
+          placeholder="Language"
+          data={getLanguageOptions()}
+          onChange={() => {
+            handleResets(form);
+          }}
+        />
+        {!!form.watch('language') && (
+          <>
+            <RestConfigForm form={form} />
+            <GRPCConfigForm form={form} />
+          </>
+        )}
+        <Button type="submit" mt="lg">
+          Submit
+        </Button>
+      </form>
+    </>
+  );
 }
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,51 +1,51 @@
-import { Box, Tabs, Text } from '@mantine/core'
-import { IconFileSettings, IconGitBranch } from '@tabler/icons-react'
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
-import Layout from 'src/components/common/layout/Layout'
-import User from 'src/components/user'
-import classes from './profile.module.css'
-import Protected from 'src/hoc/protected'
+import { Box, Tabs, Text } from '@mantine/core';
+import { IconFileSettings, IconGitBranch } from '@tabler/icons-react';
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import Layout from 'src/components/common/layout/Layout';
+import User from 'src/components/user';
+import classes from './profile.module.css';
+import Protected from 'src/hoc/protected';
 
 interface ProfileProps {}
 const Profile: React.FC<ProfileProps> = () => {
-	const [searchParams] = useSearchParams()
-	return (
-		<Protected>
-			<Layout>
-				<Box className={classes.box}>
-					<Text variant='text' size='xl' fw='bolder'>
-						Profile
-					</Text>
-					<Tabs
-						defaultValue={searchParams.get('activeTab') || 'projects'}
-						orientation='vertical'
-						activateTabWithKeyboard
-						classNames={{
-							tab: classes.tab,
-							root: classes.root,
-							list: classes.list,
-						}}
-					>
-						<Tabs.List>
-							<Tabs.Tab value='projects' leftSection={<IconFileSettings />}>
-								Projects
-							</Tabs.Tab>
-							<Tabs.Tab value='gitPlatforms' leftSection={<IconGitBranch />}>
-								GIT Platforms
-							</Tabs.Tab>
-						</Tabs.List>
+  const [searchParams] = useSearchParams();
+  return (
+    <Protected>
+      <Layout>
+        <Box className={classes.box}>
+          <Text variant="text" size="xl" fw="bolder">
+            Profile
+          </Text>
+          <Tabs
+            defaultValue={searchParams.get('activeTab')}
+            orientation="vertical"
+            activateTabWithKeyboard
+            classNames={{
+              tab: classes.tab,
+              root: classes.root,
+              list: classes.list
+            }}
+          >
+            <Tabs.List>
+              <Tabs.Tab value="projects" leftSection={<IconFileSettings />}>
+                Projects
+              </Tabs.Tab>
+              <Tabs.Tab value="gitPlatforms" leftSection={<IconGitBranch />}>
+                GIT Platforms
+              </Tabs.Tab>
+            </Tabs.List>
 
-						<Tabs.Panel value='projects'>
-							<User.Projects />
-						</Tabs.Panel>
-						<Tabs.Panel value='gitPlatforms'>
-							<User.GitPlatforms />
-						</Tabs.Panel>
-					</Tabs>
-				</Box>
-			</Layout>
-		</Protected>
-	)
-}
-export default Profile
+            <Tabs.Panel value="projects">
+              <User.Projects />
+            </Tabs.Panel>
+            <Tabs.Panel value="gitPlatforms">
+              <User.GitPlatforms />
+            </Tabs.Panel>
+          </Tabs>
+        </Box>
+      </Layout>
+    </Protected>
+  );
+};
+export default Profile;
