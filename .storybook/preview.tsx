0afb13fe1fa851a05d07450d33d42b3d13694a64
Pavan Chindukuri <cpavan@intelops.dev>
2024-03-06 12:48:20 +0530

    removed 'any' usage and added respective types

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,33 +1,33 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css'
+import '@mantine/core/styles.css';
 
-import React, { useEffect } from 'react'
-import { addons } from '@storybook/preview-api'
-import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode'
-import { MantineProvider, useMantineColorScheme } from '@mantine/core'
-import theme from '../src/theme'
+import React, { ReactNode, useEffect } from 'react';
+import { addons } from '@storybook/preview-api';
+import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
+import { MantineProvider, useMantineColorScheme } from '@mantine/core';
+import theme from '../src/theme';
 
-const channel = addons.getChannel()
+const channel = addons.getChannel();
 
 function ColorSchemeWrapper({ children }: { children: React.ReactNode }) {
-	const { setColorScheme } = useMantineColorScheme()
-	const handleColorScheme = (value: boolean) =>
-		setColorScheme(value ? 'dark' : 'light')
+  const { setColorScheme } = useMantineColorScheme();
+  const handleColorScheme = (value: boolean) =>
+    setColorScheme(value ? 'dark' : 'light');
 
-	useEffect(() => {
-		channel.on(DARK_MODE_EVENT_NAME, handleColorScheme)
-		return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme)
-	}, [channel])
+  useEffect(() => {
+    channel.on(DARK_MODE_EVENT_NAME, handleColorScheme);
+    return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme);
+  }, [channel]);
 
-	return <>{children}</>
+  return <>{children}</>;
 }
 
 export const decorators = [
-	(renderStory: any) => (
-		<ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>
-	),
-	(renderStory: any) => (
-		<MantineProvider theme={theme}>{renderStory()}</MantineProvider>
-	),
-]
+  (renderStory: () => ReactNode) => (
+    <ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>
+  ),
+  (renderStory: () => ReactNode) => (
+    <MantineProvider theme={theme}>{renderStory()}</MantineProvider>
+  )
+];
